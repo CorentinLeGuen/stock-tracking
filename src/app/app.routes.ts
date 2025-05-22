62e7af5f8be579aa9cd5fbf1aa1400beeb11dd7d
CorentinLeGuen <leguen.corentin@protonmail.com>
2025-05-22 14:51:00 -0400

    Add stocks & dashboard components and tailwindcss

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'stocks',
+        loadChildren: () =>
+            import('./features/stocks/stocks.routes').then(m => m.STOCKS_ROUTES),
+    },
+    {
+        path: 'dashboard',
+        loadChildren: () =>
+            import('./features/dashboard/dashboard.routes').then(m => m.DASHBOARD_ROUTES),
+    },
+    {
+        path: '',
+        redirectTo: 'stocks',
+        pathMatch: 'full',
+    },
+];
---
+++ src/app/features/stocks/stocks.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { StockListComponent } from './stock-list.component';
+
+export const STOCKS_ROUTES: Routes = [
+    { path: '', component: StockListComponent },
+];
